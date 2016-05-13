a210edf28247af2cda54be6bde915d3770449788
mizeramaciej <mizeramaciej@gmail.com>
2016-05-13 21:45:49 +0200

    Create checkbadanie.sh

---
+++ checkbadanie.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+a=(0.2 1 0.2 0.2 0.2 0.3)
+b=(15 15 12 15 17)
+c=(5 5 5 2 5 6)
+d=(50 50 50 50 54 52)
+e=(10 10 10 10 10 5)
+f=(3 3 3 3 4 6)
+
+result=(0 1 1 1 1 1)
+
+for i in $(seq 0 5); do
+	out=$(./badanie ${a[$i]} ${b[$i]} ${c[$i]} ${d[$i]} ${e[$i]} ${f[$i]})
+	
+	if [ "$out" != "${result[$i]}" ] ; then
+		echo "in" ${a[$i]} ${b[$i]} ${c[$i]} ${d[$i]} ${e[$i]} ${f[$i]} 
+		echo "real out" $out
+		echo "expected out" ${result[$i]}
+	fi
+done
